2ad89ba6a6fe2c185546c27bb3a67f71a2bf12ff
melikalihocagil <melikalihocagil@gmail.com>
2023-07-17 03:51:47 +0300

    sayflar birleştirildi

---
+++ lib/color/color_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  Color backgroundColor = Color(0xFFF5F6F8);
+  Color white = Colors.white;
+  Color title = Color.fromARGB(51, 172, 138, 138);
+  Color border = Color.fromRGBO(197, 197, 197, 1);
+  Color black = Colors.black;
+  Color box = Color.fromRGBO(255, 255, 255, 1);
+  Color grey = Colors.grey;
+  Color darkBlue = Color.fromARGB(255, 0, 86, 129);
+  Color redAccent = Colors.redAccent;
+  Color antiqueWhite = Color(0xFFEFB07D);
+  Color khmerCurry = Color(0xFFEF5454);
+  Color blackbean = Color(0XFF2B0806);
+}
---
+++ lib/screen/homePage.dart
@@ -0,0 +1,323 @@
+import 'package:bootcamp/color/color_theme.dart';
+import 'package:bootcamp/screen/profilePage.dart';
+import 'package:bootcamp/screen/timer.dart';
+import 'package:bootcamp/screen/toDo.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 1) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ProfilePage()),
+        );
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _showPopup();
+    });
+  }
+
+  void _showPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text('Zeliha buraya motivasyon sözü pls :D'),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text('✅'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: ColorTheme().backgroundColor,
+        leading: null,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                    height: 100,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().antiqueWhite.withOpacity(0.9),
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.auto_graph_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Analiz',
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  TimerPage()), //analiz sayfasına bağlanacak
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().khmerCurry,
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.checklist_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Konu\nTakibi',
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  TimerPage()), //konutakip sayfasına bağlanacak
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().khmerCurry,
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.assignment_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'To-do',
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  toDoListe()), // To-do sayfasına bağlanacak
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().antiqueWhite.withOpacity(0.9),
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.alarm_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Geri\nSayım',
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => TimerPage()),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().antiqueWhite.withOpacity(0.9),
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.question_mark_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Çok\nYakında',
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: ColorTheme().khmerCurry,
+                        minimumSize: Size(double.infinity, 200),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.question_mark_outlined,
+                            size: 30,
+                          ), // İstediğiniz bir icon
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Çok\nYakında',
+                            style: TextStyle(fontSize: 25),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: ColorTheme().blackbean,
+      ),
+    );
+  }
+}
